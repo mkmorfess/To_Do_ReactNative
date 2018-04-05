53c30ff4a7f36a95bc0ee7bd2116cf814059ecef
Michael Morfessis <Michael.Morfessis@globalinformationsystems.com>
2018-04-05 12:03:08 -0500

    Updated

--- app/components/Component1.js
+++ app/components/Component1.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-cards';
 
 export default class Component1 extends React.Component {
 
@@ -17,17 +18,25 @@ export default class Component1 extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.textInput}>
-          
+        <View style={styles.textInput}><Text style={styles.theText}>Test1</Text></View>
+        <View style={styles.textInput}><Text style={styles.theText}>Test2</Text></View>
+        <View style={styles.textInput}><Text style={styles.theText}>Test3</Text></View>
+        {/* <Card style={styles.textInput}>
+            <CardContent text="To-Do List!" />
             <TextInput 
-              style={styles.newInput}
               placeholder="Add to your To-Do List"
               onChangeText={(text) => this.setState({ text })}
               />
-            <TouchableOpacity style={styles.submitButton} onPress={() => this.onHandleSubmit()}>
-              <Text style={styles.submitButtonText}> Submit </Text>
-            </TouchableOpacity>
-        </View>
+            <CardAction 
+              separator={true} 
+              inColumn={false}>
+              <CardButton
+                onPress={() => this.onHandleSubmit()}
+                title="Submit"
+                color="#FEB557"
+              />
+            </CardAction>
+          </Card> */}
       </View>
     );
   }
@@ -38,6 +47,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#87cefa',
     alignSelf: 'stretch'
+  },
+  theText: {
+    backgroundColor: '#FEB557',
+    marginTop: 25,
+    justifyContent: 'space-between'
   },
   textInput: {
     flex: 1
